0b75b9e623fef799e5146cb6388afd002098aba6
Cristian <cristianferrari95@gmail.com>
2022-06-26 22:27:20 +0200

    comment main

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ reads = utils.read_input(path)
 
 bf = bf.BloomFilter(reads, k)
 
-
+#Add random picker of r to search for
+#Add output onto text file with K parameter (idk which one she means tho)
+#Just put both and a bunch of other stuff as well ez
 test = reads[150]
 print(test)
 
