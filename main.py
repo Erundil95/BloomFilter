d51f2d119090cfa6f671b17bc1649650eaff4c51
Cristian <cristianferrari95@gmail.com>
2022-06-28 20:51:49 +0200

    added k in output file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 import utils
 import bloomfilter as bf
 
-k = 25
+k = 20
 
 path = Path(__file__) / "../sample_1.fa"
 reads = utils.read_input(path)
@@ -15,13 +15,14 @@ x = randint(0, len(reads))
 test = reads[x]
 
 output = open('output.txt', "w")
+output.write('K = ' + str(k) + '\n')
 output.write('Read (r) randomly chosen from sample_1.fa: ' + '\n')
 output.write(test + '\n')
 output.write('Result: \n')
 output.write(str(bf.checkRead(test)))
 output.write('\n\n')
 
-output.write('Read (r) definitely not present in filter: ' + '\n')
+output.write('Read (r) for non-presence test in filter: ' + '\n')
 #Given the random choosing of the test string, 
 #this case test might rarely return a positive result
 test_fail = 'AGT' + test[1:]
